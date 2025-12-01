85ad8b677e6264be61fe0aea7d261cd19e567c25
Shurrasco23 <alcarras@umag.cl>
2025-11-30 22:10:39 -0300

    Commit 2: Forma de menu

---
+++ navegacion.js
@@ -0,0 +1,5 @@
+document.getElementById("boton_juego").addEventListener("click", function(){
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("Juego").style.displat = "block";
+});
+
